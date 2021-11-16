35d6949ff9920d59699d8118a56be765116397e0
Thor W <6273158@rts-365.dk>
2021-11-16 22:32:26 +0100

    working product

--- app.py
+++ app.py
@@ -3,17 +3,21 @@
 import os
 import speech_recognition as sr
 import keyboard
+import requests
 
 # Environment variable
 from dotenv import load_dotenv
 load_dotenv()
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+# WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+CHANNEL_ID = os.getenv("CHANNEL_ID")
+USER_AUTH = os.getenv("USER_AUTH")
 
 # Sending to Discord
-from discord import Webhook, RequestsWebhookAdapter
+# import discord
+# client = discord.Client()
+# from discord import Webhook, RequestsWebhookAdapter
 
-
-def recognize_speech_from_mic(recognizer, microphone):
+def recognizeSpeechFromMic(recognizer, microphone):
     with microphone as source:
         recognizer.adjust_for_ambient_noise(source)
         audio = recognizer.listen(source)
@@ -28,12 +32,32 @@ def recognize_speech_from_mic(recognizer, microphone):
         print("Unable to recognize speech")
 
 def discordHandler(phrase):
-    if phrase.startswith("play") or phrase.startswith("skip") or phrase.startswith("stop") or phrase.startswith("disconnect"):
+    if phrase.startswith("play") or phrase.startswith("skip") or phrase.startswith("stop") or phrase.startswith("resume") or phrase.startswith("disconnect"):
         newPhrase = "." + phrase
         print(newPhrase)
 
-        webhook = Webhook.from_url(WEBHOOK_URL, adapter=RequestsWebhookAdapter())
-        webhook.send(content=newPhrase)
+        # webhook = Webhook.from_url(WEBHOOK_URL, adapter=RequestsWebhookAdapter())
+        # webhook.send(content=newPhrase)
+
+        payload = {
+            "content": newPhrase
+        }
+
+        header = {
+            "authorization": USER_AUTH
+        }
+
+        r = requests.post("https://discord.com/api/v8/channels/" + CHANNEL_ID + "/messages", data=payload, headers=header)
+
+        # @client.event
+        # async def on_message(phrase):
+        #     print("Inside discord func")
+        #     channel = client.get_channel("")
+        #     message = await channel.send(phrase)
+
+        # client.run("")
+
+        # on_message(newPhrase)
         
     else:
         print("Not relevant for Discord")
@@ -44,4 +68,4 @@ while True:
 
         recognizer = sr.Recognizer()
         microphone = sr.Microphone()
-        recognize_speech_from_mic(recognizer, microphone)
+        recognizeSpeechFromMic(recognizer, microphone)
